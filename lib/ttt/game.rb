45e7e6b4b22f7221cb77026eddb50f076d5e00c3
Hugo Peixoto <hugo.peixoto@ndrive.com>
2015-10-31 12:59:24 +0000

    Refactors Game loop

--- lib/ttt/game.rb
+++ lib/ttt/game.rb
@@ -10,18 +10,33 @@ module Ttt
     end
 
     def run
-      print_board
       until board.finished?
-
-        puts "Player #{board.current_player} [1-9]:"
-
-        move = Ttt::Utils.position_to_coordinates(gets.chomp.to_i - 1)
-
-        @board = board.move(move[0], move[1])
-
         print_board
+
+        move = prompt_move
+
+        make_move move
       end
 
+      print_board
+      print_result
+    end
+
+    def prompt_move
+      puts "Player #{board.current_player} [1-9]:"
+
+      Ttt::Utils.position_to_coordinates(gets.chomp.to_i - 1)
+    end
+
+    def make_move move
+      @board = board.move move[0], move[1]
+    end
+
+    def print_board
+      puts BoardPrinter.new(board)
+    end
+
+    def print_result
       winner = board.winner
 
       if winner
@@ -29,10 +44,6 @@ module Ttt
       else
         puts "It's a draw!"
       end
-    end
-
-    def print_board
-      puts BoardPrinter.new(board)
     end
   end
 end
